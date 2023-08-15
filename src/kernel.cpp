87bee4461c8d7fcaa467d3dc0b6d569f94595829
Raven <raven@az-raven.com>
2023-08-14 17:42:03 -0700

    Kernel: Remove unnecessary code in main().
    Signed-off-by: Raven <raven@az-raven.com>

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -11,17 +11,7 @@ namespace Cobalt::Kernel {
     void main(
         // Option<Framebuffer> primary_framebuffer
     ) {
-        // if (primary_framebuffer.has_value())
-        // {
-        //     Framebuffer fb = primary_framebuffer.value();
-
-        //     for (size_t x = 0; x < fb.width(); x++) {
-        //         for (size_t y = 0; y < fb.height(); y++) {
-        //             fb.put_pixel(Coordinate(x, y), 0x00ff00);
-        //         }
-        //         wait_a_sec();
-        //     }
-        // }
+        
 
         debugln("main", "Hello, world!");
 
